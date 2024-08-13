35bd999dc6f8ef1a6eb6bb4f62dccfad7f6dded4
hoicryu <hoicryu@gmail.com>
2024-08-14 00:32:19 +0900

    [Tested] img rendering and layout shift

--- src/app/items/[slug]/page.tsx
+++ src/app/items/[slug]/page.tsx
@@ -1,5 +1,7 @@
 import { getItem, getItems } from "@/app/service/items";
 import { notFound } from "next/navigation";
+import Image from "next/image";
+import cabbageImg from "/public/images/Cabbage_kimchi.jpg";
 
 export const revalidate = 3;
 
@@ -20,7 +22,18 @@ export default async function ItemShow({ params: { slug } }: Props) {
   // 서버파일에 있는 데이터 중 해당 제품의 정보를 찾아서 렌더
   const item = await getItem(slug);
   if (!item) notFound();
-  return <div>{item.title}의 상세정보!!!</div>;
+  return (
+    <>
+      <div>{item.title}의 상세정보!!!</div>
+      <Image src={cabbageImg} alt="kimchi" priority />
+      <Image
+        src={"https://images.unsplash.com/photo-1441986300917-64674bd600d8"}
+        width={300}
+        height={100}
+        alt="img"
+      />
+    </>
+  );
 }
 
 export async function generateStaticParams() {
